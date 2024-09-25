6dd44a92b6e5a52193c693f7543be7fb98536b17
DEV-5 <vasudevvedusav@gmail.com>
2024-09-25 08:41:21 +0530

    clean up

--- usb_behind_the_scenes_stm32f407_disc1_tutorial/Debug/Src/Helpers/subdir.mk
+++ usb_behind_the_scenes_stm32f407_disc1_tutorial/Debug/Src/Helpers/subdir.mk
@@ -16,7 +16,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/Helpers/%.o Src/Helpers/%.su Src/Helpers/%.cyclo: ../Src/Helpers/%.c Src/Helpers/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DSTM32F407xx -c -I../Inc -I"/home/vdev/STM32CubeIDE/workspace_1.16.1/usb_behind_the_scenes_stm32f407_disc1_tutorial/Inc/CMSIS/Include" -I"/home/vdev/STM32CubeIDE/workspace_1.16.1/usb_behind_the_scenes_stm32f407_disc1_tutorial/Inc/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DSTM32F407xx -c -I../Inc -I"/home/vdev/STM32CubeIDE/workspace_1.16.1/STM32F4_USB_Tutorial_Proj/usb_behind_the_scenes_stm32f407_disc1_tutorial/Inc/CMSIS/Include" -I"/home/vdev/STM32CubeIDE/workspace_1.16.1/STM32F4_USB_Tutorial_Proj/usb_behind_the_scenes_stm32f407_disc1_tutorial/Inc/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Src-2f-Helpers
 
